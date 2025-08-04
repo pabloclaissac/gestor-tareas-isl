cde5d7331a67041d5facc05d54ec63c6091608b0
pabloclaissac <pabloclaissac@gmail.com>
2025-08-04 16:13:20 -0400

    Add files via upload

---
+++ modelo_tarea.py
@@ -0,0 +1,20 @@
+# modelo_tarea.py
+class Tarea:
+    def __init__(self, id, estado, nombre, compromiso, terminado, delegada,
+                 fecha_inicio, plazo, fecha_realizacion, observaciones):
+        self.id = id
+        self.estado = estado
+        self.nombre = nombre
+        self.compromiso = compromiso
+        self.terminado = terminado
+        self.delegada = delegada
+        self.fecha_inicio = fecha_inicio
+        self.plazo = plazo
+        self.fecha_realizacion = fecha_realizacion
+        self.observaciones = observaciones
+
+
+
+
+
+
