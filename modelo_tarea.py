4b122304518f9f5f0910fa206f1b6273befe1a0b
pabloclaissac <pabloclaissac@gmail.com>
2025-08-04 16:18:35 -0400

    Update modelo_tarea.py

--- modelo_tarea.py
+++ modelo_tarea.py
@@ -1,7 +1,6 @@
 # modelo_tarea.py
 class Tarea:
-    def __init__(self, id, estado, nombre, compromiso, terminado, delegada,
-                 fecha_inicio, plazo, fecha_realizacion, observaciones):
+    def __init__(self, id, estado, nombre, compromiso, terminado, delegada, fecha_inicio, plazo, fecha_realizacion, observaciones):
         self.id = id
         self.estado = estado
         self.nombre = nombre
@@ -12,6 +11,7 @@ class Tarea:
         self.plazo = plazo
         self.fecha_realizacion = fecha_realizacion
         self.observaciones = observaciones
+
 
 
 
